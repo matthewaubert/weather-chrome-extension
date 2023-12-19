fb9a014b40ff203879dcb9be97c404d385924322
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-12-19 17:15:46 -0500

    Refactor sevice worker to use chrome.storage

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,9 +1,5 @@
 /* global chrome */
-import {
-  storageAvailable,
-  deserializeSystem,
-  deserializeLocation,
-} from './local-storage.js';
+import { storageAvailable, deserialize } from './chrome-storage.js';
 import System from './classes/system.js';
 import getWeatherData from './weather-data.js';
 import setBadge from './action.js';
@@ -15,29 +11,25 @@ chrome.runtime.onInstalled.addListener(async ({ reason }) => {
   }
 
   await chrome.alarms.create('badge-update', {
-    delayInMinutes: 1,
-    periodInMinutes: 1,
+    delayInMinutes: 20,
+    periodInMinutes: 20,
   });
 });
 
 chrome.alarms.onAlarm.addListener(async () => {
-  console.log(storageAvailable('localStorage'));
-  if (storageAvailable('localStorage')) {
-    console.log('hello world');
-    // get system from localStorage
-    const system = localStorage.getItem('wceSystem')
-      ? System.getNewSystem(deserializeSystem())
+  if (storageAvailable('local')) {
+    // get system from chrome.storage.local
+    const system = (await deserialize('wceSystem'))
+      ? System.getNewSystem(await deserialize('wceSystem'))
       : System.getNewSystem('imperial');
-    // get location from localStorage
-    const location = localStorage.getItem('wceLocation')
-      ? deserializeLocation()
+    // get location from chrome.storage.local
+    const location = (await deserialize('wceLocation'))
+      ? await deserialize('wceLocation')
       : 'philadelphia';
 
     const weatherData = await getWeatherData(location); // get weather
     if (!weatherData.error) {
       setBadge(weatherData.current, system); // set badge
-
-      console.log('updated!');
     }
   }
 });
